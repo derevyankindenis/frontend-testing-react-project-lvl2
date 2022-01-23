1f29958fb12fde9d87795ad04dea7cc5f1298c42
WrinkleJ <WrinkleJ@github.com>
2022-01-23 17:12:41 +0300

    test without pageObject

--- __tests__/TodoApp.test.jsx
+++ __tests__/TodoApp.test.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import App from '@hexlet/react-todo-app-with-backend';
-import { render, screen } from '@testing-library/react';
-import TodoApp from '../__testPages__/TodoApp';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+  waitFor,
+} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import server from '../mocks/server';
-
-const page = new TodoApp(screen);
 
 beforeEach(() => {
   server.listen({ onUnhandledRequest: 'warn' });
@@ -17,124 +20,168 @@ afterEach(() => {
   server.close();
 });
 
+function addTask(name) {
+  const input = screen.queryByRole('textbox', {
+    name: /new task/i,
+  });
+  const btn = screen.getByRole('button', {
+    name: 'Add',
+  });
+  userEvent.type(input, name);
+  userEvent.click(btn);
+}
+
+function addList(name) {
+  const input = screen.queryByRole('textbox', {
+    name: /new list/i,
+  });
+
+  const btn = screen.getByRole('button', {
+    name: /add list/i,
+  });
+
+  userEvent.type(input, name);
+  userEvent.click(btn);
+}
+
 describe('Todo app', () => {
-  test('there are inputs for tasks and lists', async () => {
-    expect(page.taskInput).toBeVisible();
-    expect(page.listInput).toBeVisible();
-  });
+  test('user can add task', async () => {
+    addList('list 1');
+    await screen.findByText(/list 1/i);
 
-  test('there are no tasks before user add its', async () => {
-    const tasks = await page.getTasks();
-    expect(tasks).toHaveLength(0);
-  });
+    const input = screen.queryByRole('textbox', { name: /new task/i });
+    const submitBtn = screen.getByRole('button', { name: 'Add' });
 
-  test('there is placeholder for empty tasks', () => {
-    expect(page.tsksEmptyText).toBeVisible();
-  });
+    userEvent.type(input, 'task 1');
+    userEvent.click(submitBtn);
 
-  test('user can add tasks', async () => {
-    await page.addList('new list');
-    await page.addTask('new task');
-    await page.addTask('new task 2');
-    const task = await page.getTask('new task');
-    const task2 = await page.getTask('new task 2');
-    expect(task).toBeVisible();
-    expect(task2).toBeVisible();
+    expect(input).toHaveAttribute('readonly');
+    expect(submitBtn).toBeDisabled();
+    expect(await screen.findByText(/task 1/i)).toBeInTheDocument();
+    expect(input).not.toHaveAttribute('readonly');
+    expect(submitBtn).toBeEnabled();
   });
 
   test('user can remove tasks', async () => {
-    await page.addList('new list');
-    const deleteFirstTask = await page.addTask('first task');
-    await page.addTask('second task');
-    await deleteFirstTask();
-    const task = await page.getTask('first task');
-    const task2 = await page.getTask('second task');
-    expect(task).toBeNull();
-    expect(task2).toBeVisible();
+    addList('list 1');
+    await screen.findByText(/list 1/i);
+    addTask('task 1');
+    await screen.findByText(/task 1/i);
+    const delTask1Btn = await screen.findByRole('button', { name: 'Remove' });
+    addTask('task 2');
+    await screen.findByText(/task 2/i);
+    userEvent.click(delTask1Btn);
+    await waitForElementToBeRemoved(screen.queryByText(/task 1/i));
+    expect(screen.queryByText(/task 1/i)).not.toBeInTheDocument();
+    expect(await screen.findByText(/task 2/i)).toBeInTheDocument();
   });
 
   test('user can complete tasks', async () => {
-    await page.addList('new list');
-    await page.addTask('first task');
-    await page.addTask('second task');
-    await page.completeTask('first task');
-    const task = await page.getTask('first task');
-    const task2 = await page.getTask('second task');
-    expect(task).toBeChecked();
-    expect(task2).not.toBeChecked();
-  });
-
-  test('user can add lists', async () => {
-    await page.addList('new list');
-    await page.addList('new list 2');
-    const list = await page.getList('new list');
-    const list2 = await page.getList('new list 2');
-    expect(list).toBeVisible();
-    expect(list2).toBeVisible();
-  });
-
-  test('user can remove lists', async () => {
-    const deleteFirstList = await page.addList('first list');
-    await page.addList('second list');
-    await deleteFirstList();
-    const list = await page.getList('first list');
-    const list2 = await page.getList('second list');
-    expect(list).toBeNull();
-    expect(list2).toBeVisible();
-  });
-
-  test('each task in own list', async () => {
-    await page.addList('list 1');
-    await page.addTask('task 1');
-    await page.addTask('task 2');
-    await page.addList('list 2');
-    expect(page.tsksEmptyText).toBeVisible();
-    await page.addTask('task 3');
-    await page.addTask('task 4');
-    await page.pickList('list 1');
-    const task1 = await page.getTask('task 1');
-    const task2 = await page.getTask('task 2');
-    expect(task1).toBeVisible();
-    expect(task2).toBeVisible();
-    await page.pickList('list 2');
-    const task3 = await page.getTask('task 3');
-    const task4 = await page.getTask('task 4');
-    expect(task3).toBeVisible();
-    expect(task4).toBeVisible();
-  });
-
-  test("removing tasks in list doesn't affect on other list", async () => {
-    await page.addList('list 1');
-    await page.addList('list 2');
-    await page.addTask('task 1');
-    await page.pickList('list 1');
-    const removeTask3 = await page.addTask('task 3');
-    await removeTask3();
-    expect(page.tsksEmptyText).toBeVisible();
-    await page.pickList('list 2');
-    const task1 = await page.getTask('task 1');
-    expect(task1).toBeVisible();
-  });
-
-  test("deleted tasks aren't restored", async () => {
-    const removeList = await page.addList('list 1');
-    await page.addTask('task 1');
-    await removeList();
-    await page.addList('list 1');
-    const task = await page.getTask('task 1');
-    expect(task).toBeNull();
-  });
-
-  test("user can't create lists with same name", async () => {
-    await page.addList('list 1');
-    await page.addList('list 1');
-    expect(page.getErrorMesage('list 1')).toBeVisible();
+    addList('list 1');
+    await screen.findByText(/list 1/i);
+    addTask('task 1');
+    await screen.findByText(/task 1/i);
+    addTask('task 2');
+    await screen.findByText(/task 2/i);
+    const checkBoxTask1 = screen.getByRole('checkbox', { name: 'task 1' });
+    userEvent.click(checkBoxTask1);
+    await waitFor(() => expect(checkBoxTask1).toBeChecked());
+    expect(screen.getByRole('checkbox', { name: 'task 2' })).not.toBeChecked();
   });
 
   test("user can't create task with same name", async () => {
-    await page.addList('list 1');
-    await page.addTask('task 1');
-    await page.addTask('task 1');
-    expect(page.getErrorMesage('task 1')).toBeVisible();
+    addList('list 1');
+    await screen.findByText(/list 1/i);
+    addTask('task 1');
+    await screen.findByText(/task 1/i);
+    addTask('task 1');
+    await waitFor(() => expect(screen.getByText(/task 1 already exists/i)).toBeVisible());
+    // eslint-disable-next-line jest-dom/prefer-in-document
+    expect(screen.getAllByRole('checkbox', { name: /task 1/i })).toHaveLength(1);
+  });
+
+  test('user can add lists', async () => {
+    const input = screen.queryByRole('textbox', {
+      name: /new list/i,
+    });
+
+    const submitBtn = screen.getByRole('button', {
+      name: /add list/i,
+    });
+
+    userEvent.type(input, 'list 1');
+    userEvent.click(submitBtn);
+
+    expect(input).toHaveAttribute('readonly');
+    expect(submitBtn).toBeDisabled();
+    expect(await screen.findByText(/list 1/i)).toBeInTheDocument();
+    expect(input).not.toHaveAttribute('readonly');
+    expect(submitBtn).toBeEnabled();
+  });
+
+  test('user can remove lists', async () => {
+    addList('list 1');
+    await screen.findByText(/list 1/i);
+    const removeBtn = screen.getByRole('button', { name: /remove list/i });
+    addList('list 2');
+    userEvent.click(removeBtn);
+    await waitForElementToBeRemoved(screen.queryByText(/list 1/i));
+    expect(screen.queryByText(/list 1/i)).not.toBeInTheDocument();
+    expect(await screen.findByText(/list 2/i)).toBeInTheDocument();
+  });
+
+  test("user can't create lists with same name", async () => {
+    addList('list 1');
+    await screen.findByText(/list 1/i);
+    addList('list 1');
+    await waitFor(() => expect(screen.getByText(/list 1 already exists/i)).toBeVisible());
+    // eslint-disable-next-line jest-dom/prefer-in-document
+    expect(screen.getAllByRole('button', { name: /list 1/i })).toHaveLength(1);
+  });
+
+  test("deleted tasks aren't restored", async () => {
+    addList('list 1');
+    await screen.findByText(/list 1/i);
+    addTask('task 1');
+    await screen.findByText(/task 1/i);
+    userEvent.click(screen.getByRole('button', { name: /remove list/i }));
+    await waitForElementToBeRemoved(screen.queryByText(/list 1/i));
+    addList('list 1');
+    await screen.findByText(/list 1/i);
+    expect(screen.queryByText(/task 1/i)).not.toBeInTheDocument();
+  });
+
+  test("removing tasks in list doesn't affect on other list", async () => {
+    addList('list 1');
+    const list1 = await screen.findByRole('button', { name: /list 1/i });
+    addList('list 2');
+    const list2 = await screen.findByRole('button', { name: /list 2/i });
+    addTask('task 1');
+    await screen.findByText(/task 1/i);
+    userEvent.click(list1);
+    addTask('task 3');
+    await screen.findByText(/task 3/i);
+    const delTask1Btn = await screen.findByRole('button', { name: 'Remove' });
+    userEvent.click(delTask1Btn);
+    await waitForElementToBeRemoved(screen.queryByText(/task 3/i));
+    userEvent.click(list2);
+    expect(await screen.findByText(/task 1/i)).toBeInTheDocument();
+  });
+
+  test('each task in own list', async () => {
+    addList('list 1');
+    const list1 = await screen.findByRole('button', { name: /list 1/i });
+    addTask('task 1');
+    await screen.findByText(/task 1/i);
+    addList('list 2');
+    await screen.findByRole('button', { name: /list 2/i });
+    expect(screen.getByText('Tasks list is empty')).toBeVisible();
+    addTask('task 2');
+    await screen.findByText(/task 2/i);
+    expect(screen.queryByText(/task 1/i)).not.toBeInTheDocument();
+    expect(screen.getByText(/task 2/i)).toBeInTheDocument();
+    userEvent.click(list1);
+    expect(screen.queryByText(/task 2/i)).not.toBeInTheDocument();
+    expect(screen.getByText(/task 1/i)).toBeInTheDocument();
   });
 });
